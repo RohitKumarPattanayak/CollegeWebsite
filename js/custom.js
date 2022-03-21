1a7a00f872da5d66702ec78e2c9c85bae78a9a6d
Rohit <rohitkumarpattanayak@gmail.com>
2022-03-21 18:17:54 +0530

    loading and few changes

--- js/custom.js
+++ js/custom.js
@@ -29,3 +29,22 @@
     }
   });
 })(jQuery);
+
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('body', true);
+  setVisible('#loading', false);
+});
